2c15d1d4383182b665c6190d1057088b2b97119f
pooetitu <pooetitu@gmail.com>
2020-05-08 20:07:42 +0200

    Adding a drawCircle method

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 
 #include <SDL.h>
 
+#include "main.h"
 
 int main(int argv, char* argc[]) {
     
@@ -11,7 +12,7 @@ int main(int argv, char* argc[]) {
         printf("SDL initialisation error %s", SDL_GetError());
         return EXIT_FAILURE;
     }
-    
+    SDL_Renderer* renderer = NULL;
     SDL_Window* window = NULL;
     window = SDL_CreateWindow("Game screen", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_RESIZABLE);
     if (window == NULL)
@@ -19,10 +20,25 @@ int main(int argv, char* argc[]) {
         printf("Window initialisation error  %s", SDL_GetError());
         return EXIT_FAILURE;
     }
-    
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == NULL)
+    {
+        printf("Renderer initialisation error  %s", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    Uint8 r=255, g=0, b=255;
+    SDL_SetRenderDrawColor(renderer,r,g,b,SDL_ALPHA_OPAQUE);
+    drawCircle(150, 0, 0, renderer);
+    SDL_RenderPresent(renderer);
     SDL_Delay(3000);
 	
     return 0;
 }
 
-
+void drawCircle(int radius, int x, int y, SDL_Renderer *renderer) {
+    for (float i = 0.00f; i < 360.00f; i += 0.01f) {
+        float angle = i * M_PI / 180;
+        SDL_RenderDrawPoint(renderer, radius * sin(angle) + x + radius, radius * cos(angle) + y + radius);
+    }
+}
--- src/main.h
+++ src/main.h
@@ -1,6 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <SDL.h>
 
 
+void drawCircle(int radius, int x, int y, SDL_Renderer *renderer);
 int main(int argv, char* argc[]);
