1aa1815c392fbb52a42f8f52fb172145c5924119
pooetitu <pooetitu@gmail.com>
2020-05-10 01:44:43 +0200

    Allowing players to move over the game scene.

--- src/main.c
+++ src/main.c
@@ -12,9 +12,16 @@ struct Player {
     Uint8 r;
     Uint8 g;
     Uint8 b;
+    SDL_Scancode up;
+    SDL_Scancode down;
+    SDL_Scancode left;
+    SDL_Scancode right;
 };
 
+
 int main(int argv, char* argc[]) {
+    int h = WINDOW_HEIGHT;
+    int w = WINDOW_WIDTH;
     
     if (SDL_Init(SDL_INIT_VIDEO))
     {
@@ -35,34 +42,70 @@ int main(int argv, char* argc[]) {
         printf("Renderer initialisation error  %s", SDL_GetError());
         return EXIT_FAILURE;
     }
-    int w;
-    int h;
-    SDL_GetWindowSize(window, &w, &h);
+
+    SDL_SetWindowResizable(window, SDL_FALSE);
+
+
 
     printf("%d %d", w, h);
-    Player p1 = {10, 10, 10, 255, 0, 0};
-    Player p2 = {w-10, 10, 10, 0, 0, 255};
-    Uint8 r = 0, g = 0, b = 0;
-    while (1) {
+    Player p1 = {10, h-10, 10, 255, 0, 0,SDL_SCANCODE_UP,SDL_SCANCODE_DOWN,SDL_SCANCODE_LEFT,SDL_SCANCODE_RIGHT};
+    Player p2 = {w-10, h-10, 10, 0, 0, 255,SDL_SCANCODE_Z,SDL_SCANCODE_S,SDL_SCANCODE_Q,SDL_SCANCODE_D};
+    Uint8 r=0, g=0, b=0;
+    SDL_Event event;
+    int quit = 0;
+
+
+    while (!quit) {
+        SDL_Delay(10);
         SDL_SetRenderDrawColor(renderer, r, g, b, SDL_ALPHA_OPAQUE);
         SDL_RenderClear(renderer);
-        p1.x++;
-        p1.y++;
+
+        UpdatePlayer(&p1, renderer);
+        UpdatePlayer(&p2, renderer);
 
 
-        SDL_SetRenderDrawColor(renderer, p1.r, p1.g, p1.b, SDL_ALPHA_OPAQUE);
-        drawCircle(p1.size, p1.x, p1.y, renderer);
-
-        SDL_SetRenderDrawColor(renderer, p2.r, p2.g, p2.b, SDL_ALPHA_OPAQUE);
-        drawCircle(p2.size, p2.x, p2.y, renderer);
         SDL_RenderPresent(renderer);
+
+        if (!SDL_PollEvent(&event)) continue;
+
+        switch (event.type) {
+        case SDL_QUIT:
+            quit = SDL_TRUE;
+            break;
+        }
     }
-	
     return 0;
 }
 
+void UpdatePlayer(Player* player,SDL_Renderer* renderer) {
+    SDL_SetRenderDrawColor(renderer, player->r, player->g, player->b, SDL_ALPHA_OPAQUE);
+    DrawCircle(player->size, player->x, player->y, renderer);
+    UpdatePlayerPosition(player);
+}
 
-void drawCircle(int radius, int x, int y, SDL_Renderer *renderer) {
+void UpdatePlayerPosition(Player* player) {
+    int h = WINDOW_HEIGHT;
+    int w = WINDOW_WIDTH;
+    const Uint8* keyboard_state_array = SDL_GetKeyboardState(NULL);
+    if (!keyboard_state_array[player->down] && keyboard_state_array[player->up]) {
+        if (player->y - player->size > 0 )
+        player->y--;
+    }    
+    if (keyboard_state_array[player->down] && !keyboard_state_array[player->up]) {
+        if (player->y + player->size < h)
+        player->y++;
+    }
+    if (!keyboard_state_array[player->left] && keyboard_state_array[player->right]) {
+        if (player->x + player->size < w)
+        player->x++;
+    }    
+    if (keyboard_state_array[player->left] && !keyboard_state_array[player->right]) {
+        if (player->x - player->size > 0)
+        player->x--;
+    }
+}
+
+void DrawCircle(int radius, int x, int y, SDL_Renderer *renderer) {
     for (float i = 0.00f; i < 360.00f; i += 0.01f) {
         float angle = i * M_PI / 180;
         SDL_RenderDrawPoint(renderer, radius * sin(angle) + x, radius * cos(angle) + y);
--- src/main.h
+++ src/main.h
@@ -3,7 +3,12 @@
 
 #include <SDL.h>
 
+#define WINDOW_HEIGHT 600;
+#define WINDOW_WIDTH 800;
+
 typedef struct Player Player;
 
-void drawCircle(int radius, int x, int y, SDL_Renderer *renderer);
+void UpdatePlayer(Player* player, SDL_Renderer* renderer);
+void UpdatePlayerPosition(Player* player);
+void DrawCircle(int radius, int x, int y, SDL_Renderer *renderer);
 int main(int argv, char* argc[]);
